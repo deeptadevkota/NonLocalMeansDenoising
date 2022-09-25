fdef5207a526041729a0b5c5ea4f7f3bddeb7d82
akanksha <55958454+akanksha-00@users.noreply.github.com>
2022-09-25 15:51:59 +0530

    updated median

--- NLmeansMedianfilter.m
+++ NLmeansMedianfilter.m
@@ -7,11 +7,7 @@ function [output]=NLmeansMedianfilter(input,s,t,f,h)
  %  f: ratio of similarity window
  %  h: degree of filtering
  %
- %  Author: Jose Vicente Manjon Herrera & Antoni Buades
- %  Date: 09-03-2006
- %
- %  Implementation of the Non local filter proposed for A. Buades, B. Coll and J.M. Morel in
- %  "A non-local algorithm for image denoising"
+ %  Implementation of the Non local means filter using median
  %
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -58,14 +54,9 @@ function [output]=NLmeansMedianfilter(input,s,t,f,h)
                 if(r==i1 && s==j1) continue; end;
                                 
                 W2= filteredimage2(r-f:r+f , s-f:s+f);
-                
-%                  ImToPolar (kernel.*W2,0, 1, 3, 3);
-%                   ImToPolar (kernel.*W1, 0, 1,3, 3);
-               % d = dist_fractional ( W1 , W2 , 2 );
 
                 d = sum(sum(kernel.*(W1-W2).*(W1-W2)));
-%                 d1 = conv2(kernel, (W1-W2).*(W1-W2),'same');  
-%                 d = sum(d1(:));
+
                 w=exp(-d/h);    
                   
                                  
@@ -88,7 +79,7 @@ function [output]=NLmeansMedianfilter(input,s,t,f,h)
             output(i,j) = input(i,j);
         end                
  end
- %i
+ 
  end
  
 function [kernel] = make_kernel(f)              
